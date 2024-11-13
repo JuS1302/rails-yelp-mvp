a0551b521724de1b07e2bfff3c9af94fb0697fba
Julie Simon <sim.ju@live.fr>
2024-11-13 12:05:21 +0100

    routes restaurants created

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   RATINGS = (0..5)
   validates :rating, inclusion: { in: RATINGS }
+  validates :rating, numericality: { only_integer: true }
   validates :content, :rating, presence: true
-  validates :number_field, numericality: { only_integer: true }
 end
